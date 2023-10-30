1e1b2b642eb1e8bf5d0a88f581291fb10a14337c
ArisISCute <Raymond@dsm.hs.kr>
2023-10-30 14:51:23 +0900

    Fix

---
+++ Assets/Script/GetKey.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetInput : MonoBehaviour
+{
+    private NoteTimeCheck noteTimeCheck;
+    private GameObject[] notes;
+
+    private KeyCode key1;
+    private KeyCode key2;
+    private KeyCode key3;
+    private KeyCode key4;
+
+    private void Start()
+    {
+        noteTimeCheck = GameObject.Find("NoteTimeCheck").GetComponent<NoteTimeCheck>();
+        
+        key1 = KeyCode.A;
+        key2 = KeyCode.S;
+        key3 = KeyCode.K;
+        key4 = KeyCode.L;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(key1))
+        {
+            noteTimeCheck.TapNote(1);
+        }
+
+        if (Input.GetKeyDown(key2))
+        {
+            noteTimeCheck.TapNote(2);
+        }
+        
+        if (Input.GetKeyDown(key3))
+        {
+            noteTimeCheck.TapNote(3);
+        }
+        
+        if (Input.GetKeyDown(key4))
+        {
+            noteTimeCheck.TapNote(4);
+        }
+    }
+}
