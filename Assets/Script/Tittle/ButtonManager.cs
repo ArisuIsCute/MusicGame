c1192ecd8f00fb5e184dcc695125344c7cbdc4a3
ArisISCute <Raymond@dsm.hs.kr>
2023-10-30 19:23:37 +0900

    Fix Compelete

---
+++ Assets/Script/Tittle/ButtonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
