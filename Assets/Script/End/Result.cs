fa39dbc6e6a44eb003fd219b3d2b8ded0b307f3f
ArisISCute <Raymond@dsm.hs.kr>
2023-11-02 19:57:14 +0900

    small update

--- Assets/Script/End/Result.cs
+++ Assets/Script/End/Result.cs
@@ -12,6 +12,7 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private TextMeshProUGUI goodCountText;
     [SerializeField] private TextMeshProUGUI greatCountText;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI rankText;
     
     private Score score;
 
@@ -19,6 +20,19 @@ public class NewBehaviourScript : MonoBehaviour
     {
         score = GameObject.Find("Score").GetComponent<Score>();
 
+        if (score.GreatCnt > score.GoodCnt + score.MissCnt)
+        {
+            rankText.text = "S";
+        }else if (score.GoodCnt > score.MissCnt)
+        {
+            rankText.text = "A";
+        }
+        else
+        {
+            rankText.text = "B";
+        }
+        
+        
         comboText.text = "Combo : " + score.Combo.ToString();
         maxComboText.text = "MaxCombo : " + score.MaxCombo.ToString();
         missCountText.text = "Miss : " + score.MissCnt.ToString();
