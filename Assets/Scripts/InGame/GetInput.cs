1b1e81f9b3e08b861c96e1fccaac86e6ea24ea76
ArisISCute <Raymond@dsm.hs.kr>
2023-11-24 11:31:22 +0900

    change s

--- Assets/Scripts/InGame/GetInput.cs
+++ Assets/Scripts/InGame/GetInput.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class GetInput : MonoBehaviour
 {
-    private KeyCode key1 = KeyCode.A;
-    private KeyCode key2 = KeyCode.S;
-    private KeyCode key3 = KeyCode.K;
-    private KeyCode key4 = KeyCode.L;
+    public KeyCode key1 = KeyCode.A;
+    public KeyCode key2 = KeyCode.S;
+    public KeyCode key3 = KeyCode.K;
+    public KeyCode key4 = KeyCode.L;
 
     private NoteTime noteTime;
 
